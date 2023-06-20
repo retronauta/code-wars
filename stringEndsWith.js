02adfcc5ad1c298d8578d02e36ed7a7e05a5591a
Limbert Lino <limbertlino@gmail.com>
2023-06-20 19:59:33 -0400

    Done

---
+++ stringEndsWith.js
@@ -0,0 +1,17 @@
+//* ##### INSTRUCTIONS #####
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+//* ##### EXAMPLES #####
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+//* ##### SOLUTION #####
+
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
+
+console.log(solution('abc', 'bc'));
+console.log(solution('abc', 'd'));
